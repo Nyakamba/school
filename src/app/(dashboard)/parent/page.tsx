4cce893ae8c4c86897c865b4b60695fe45545d51
Nyakamba <omwegaenock@gmail.com>
2024-12-22 20:45:25 +0300

    Build the dashboard layout

---
+++ src/app/(dashboard)/parent/page.tsx
@@ -0,0 +1,5 @@
+const Parentpage = () => {
+  return <div>Parentpage</div>;
+};
+
+export default Parentpage;
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const menuItems = [
   {
     title: "MENU",
@@ -112,3 +115,36 @@ const menuItems = [
     ],
   },
 ];
+
+const Menu = () => {
+  return (
+    <div className="mt-4 text-sm">
+      {menuItems.map((item) => (
+        <div key={item.title} className="flex flex-col gap-2">
+          <h3 className="hidden lg:block text-gray-500 font-semibold my-4">
+            {item.title}
+          </h3>
+          <ul>
+            {item.items.map((menuItem) => (
+              <Link
+                key={menuItem.label}
+                href={menuItem.href}
+                className="flex items-center justify-center lg:justify-start gap-4 text-gray-600 py-2"
+              >
+                <Image
+                  src={menuItem.icon}
+                  alt={menuItem.label}
+                  width={20}
+                  height={20}
+                />
+                <span className="hidden lg:block">{menuItem.label}</span>
+              </Link>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Menu;
