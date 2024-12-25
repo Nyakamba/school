4a9a52ecc5699627ec0b6ae2a4132de2d835084d
Nyakamba <omwegaenock@gmail.com>
2024-12-25 15:01:51 +0300

    Design teachers page

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,5 +1,26 @@
+"use client";
+
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+import EventCalendar from "@/components/EventCalendar";
+
 const TeacherPage = () => {
-  return <div>TeacherPage</div>;
+  return (
+    <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+      {/* Left */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white rounded-md p-4">
+          <h1 className="text-lg font-semibold">Schedule</h1>
+          <BigCalendar />
+        </div>
+      </div>
+
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8 ">
+        <Announcements />
+      </div>
+    </div>
+  );
 };
 
 export default TeacherPage;
