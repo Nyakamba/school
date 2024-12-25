bf1ddc37c2eefaaa16357e4b4a70ff2e12854007
Nyakamba <omwegaenock@gmail.com>
2024-12-25 15:09:34 +0300

    Design parent page

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,5 +1,25 @@
-const Parentpage = () => {
-  return <div>Parentpage</div>;
+"use client";
+
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+
+const ParentPage = () => {
+  return (
+    <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+      {/* Left */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white rounded-md p-4">
+          <h1 className="text-lg font-semibold">Schedule (John Does)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8 ">
+        <Announcements />
+      </div>
+    </div>
+  );
 };
 
-export default Parentpage;
+export default ParentPage;
