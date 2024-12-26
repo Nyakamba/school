56a2efcab4d98aa6a313fe0aec80677c3ea4ab60
Nyakamba <omwegaenock@gmail.com>
2024-12-26 22:51:12 +0300

    Build the classes list page

--- src/app/(dashboard)/list/classes/page.tsx
+++ src/app/(dashboard)/list/classes/page.tsx
@@ -1,25 +1,38 @@
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { role, subjectsData } from "@/lib/data";
+import { role, classesData } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-type Subject = {
+type Class = {
   id: number;
   name: string;
-  teachers: string[];
+  capacity: number;
+  grade: number;
+  supervisor: string;
 };
 
 const columns = [
   {
-    header: "Subject Name",
+    header: "Class Name",
     accessor: "name",
   },
   {
-    header: "Teachers",
-    accessor: "teachers",
+    header: "Capacity",
+    accessor: "capacity",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Grade",
+    accessor: "grade",
+    className: "hidden md:table-cell",
+  },
+
+  {
+    header: "Supervisor",
+    accessor: "supervisor",
     className: "hidden md:table-cell",
   },
 
@@ -28,18 +41,20 @@ const columns = [
     accessor: "actions",
   },
 ];
-const SubjectListPage = () => {
-  const renderRow = (item: Subject) => (
+const ClassListPage = () => {
+  const renderRow = (item: Class) => (
     <tr
       key={item.id}
       className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purpleLight"
     >
       <td className="flex items-center gap-4 p-4">{item.name}</td>
-      <td className="hidden md:table-cell">{item.teachers.join(", ")}</td>
+      <td className="hidden md:table-cell">{item.capacity}</td>
+      <td className="hidden md:table-cell">{item.grade}</td>
+      <td className="hidden md:table-cell">{item.supervisor}</td>
 
       <td>
         <div className="flex items-center gap-2">
-          <Link href={`/list/subjects/${item.id}`}>
+          <Link href={`/list/classs/${item.id}`}>
             <button className="w-7 h-7 flex items-center justify-center rounded-full bg-sky">
               <Image src="/view.png" alt="edit" width={16} height={16} />
             </button>
@@ -57,7 +72,7 @@ const SubjectListPage = () => {
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0 ">
       {/* Top */}
       <div className=" flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All Subjects</h1>
+        <h1 className="hidden md:block text-lg font-semibold">All Classes</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4  self-end">
@@ -77,7 +92,7 @@ const SubjectListPage = () => {
       </div>
 
       {/* List */}
-      <Table columns={columns} renderRow={renderRow} data={subjectsData} />
+      <Table columns={columns} renderRow={renderRow} data={classesData} />
       {/* Pagination */}
 
       <Pagination />
@@ -85,4 +100,4 @@ const SubjectListPage = () => {
   );
 };
 
-export default SubjectListPage;
+export default ClassListPage;
